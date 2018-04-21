d30230ba2e37ae43f2931ea2091a2b410d14a1e7
jayz2053 <jpj19@students.uwf.edu>
2018-04-21 16:05:09 -0500

    Update album art business

--- routes/covers.js
+++ routes/covers.js
@@ -1,7 +1,11 @@
 module.exports = (app, albumArt) =>{
 
 app.get('/covers/:artist/:album', (req, res) =>{
-  albumArt(req.params.artist, {album : req.params.album, size: 'small'}, (err, path) => {
+
+  var artist = req.params.artist.replace('-', ' ')
+  var album = req.params.artist.replace('-', ' ')
+
+  albumArt(req.params.artist, {album : req.params.album, size: 'medium'}, (err, path) => {
     if(err) throw err
     res.send(path)
   })

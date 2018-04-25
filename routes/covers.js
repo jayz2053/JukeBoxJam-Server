2855547bcbba68897d5406c257e2437de4421a5a
jayz2053 <jpj19@students.uwf.edu>
2018-04-25 10:36:47 -0500

    removed console log

--- routes/covers.js
+++ routes/covers.js
@@ -5,7 +5,7 @@ app.get('/covers/:artist/:album', (req, res) =>{
   var qArtist = req.params.artist.replace('-', ' ')
   var qAlbum = req.params.artist.replace('-', ' ')
 
-  console.log(artist album)
+  //console.log(artist album)
   albumArt(qArtist, {album : qAlbum, size: 'large'}, (err, path) => {
     if(err) throw err
     res.send(path)
